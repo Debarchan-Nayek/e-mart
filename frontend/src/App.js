5e26c426eb5e253eef04146acc4895fad41322ed
Debarchan Nayek <dnayek2002@gmail.com>
2021-08-06 20:50:19 +0530

    Product Page Ation & Reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import HomePage from './pages/Home';
+import CartPage from './pages/CartPage';
 import Productdetails from './pages/ProductDetails'
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
@@ -15,8 +16,9 @@ function App() {
           <Container className="mt-4">
             <Route path="/" component={HomePage} exact />
             <Route path="/product/:id" component={Productdetails} />
+            <Route path="/cart/:id?" component={CartPage} />
           </Container>
-        </main>
+        </main>  
         <Footer />
       </Router>
     </>
--- frontend/src/pages/ProductDetails/index.js
+++ frontend/src/pages/ProductDetails/index.js
@@ -1,60 +1,103 @@
-import React, { useEffect, useState } from 'react';
-import Rating from '../Product/rating';
-import { Link } from 'react-router-dom'
-import { Row, Col, ListGroup, ListGroupItem, Button, Image } from 'react-bootstrap';
-import { FaArrowAltCircleLeft } from 'react-icons/fa';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Rating from "../Product/rating";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { listProductDetails } from "../../actions/productActions";
+import {
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Button,
+  Image,
+  Form,
+} from "react-bootstrap";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import Loader from "../../components/Loader";
+import Message from "../../components/Message";
 
-const ProductDetails = ({ match }) => {
-    const [product, setProduct] = useState([])
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const {data} = await axios.get(`/api/products/${match.params.id}`);
-            setProduct(data)
-        }
-        fetchProduct();
-    },[]);
+const ProductDetails = ({ history, match }) => {
+  const [qty, setQty] = useState(1);
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+  useEffect(() => {
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
-    // const product = Products.find((p) => p._id === match.params.id);
-  return(
-    <div>
-    <Link to="/" className="btn btn-light ">
-        <FaArrowAltCircleLeft />
-        &nbsp; Go Back
-    </Link>
-        <Row>
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  }
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Link to="/" className="btn btn-light ">
+            <FaArrowAltCircleLeft />
+            &nbsp; Go Back
+          </Link>
+          <Row>
             <Col md={6}>
-                <Image src={product.image} alt={product.name} fluid/>
+              <Image src={product.image} alt={product.name} fluid />
             </Col>
             <Col md={3}>
-                <ListGroup vsriant="flush">
-                    <ListGroupItem>
-                        <h3>{product.name}</h3>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        <Rating value={product.rating} total={`${product.numReviews} Reviews`}/>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                        Price: {product.price}
-                    </ListGroupItem>
-                    <ListGroupItem>{product.description}</ListGroupItem>
-                </ListGroup>
+              <ListGroup vsriant="flush">
+                <ListGroupItem>
+                  <h3>{product.name}</h3>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Rating
+                    value={product.rating}
+                    total={`${product.numReviews} Reviews`}
+                  />
+                </ListGroupItem>
+                <ListGroupItem>Price: {product.price}</ListGroupItem>
+                <ListGroupItem>{product.description}</ListGroupItem>
+              </ListGroup>
             </Col>
             <Col md={3}>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status: </Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock :("}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              {product.countInStock > 0 && (
                 <ListGroupItem>
-                    <Row>
-                        <Col>Status: </Col>
-                        <Col>{product.countInStock > 0 ? "In Stock" : "Out of Stock :("}</Col>
-                    </Row>
+                  <Row>
+                    <Col>Qty</Col>
+                    <select
+                    default={1}
+                      value={qty}
+                      onChange={(e) => setQty(e.target.value)}
+                    >
+                      {[...Array(product.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </select>
+                  </Row>
                 </ListGroupItem>
-                <ListGroupItem>
-                    <Button className="btn-block" type="button" >Add to Cart</Button>
-                </ListGroupItem>
+              )}
+              <ListGroupItem>
+                <Button className="btn-block" type="button" onClick={addToCartHandler}>
+                  Add to Cart
+                </Button>
+              </ListGroupItem>
             </Col>
-        </Row>
-    </div>
-   )
+          </Row>
+        </>
+      )}
+    </>
+  );
+};
 
- }
-
-export default ProductDetails
+export default ProductDetails;
